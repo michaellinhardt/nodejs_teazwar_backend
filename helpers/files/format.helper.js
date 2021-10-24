cf27dab2acf4620642f62fe946e849993712af68
Michael Linhardt <linhardt.michael@gmail.com>
2021-10-25 06:19:30 +0700

    change level up display

--- helpers/files/format.helper.js
+++ helpers/files/format.helper.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-  userTwitchLevlUp: ({ display_name, level }) => `=[ @${display_name} ${level} ]=`,
+  userTwitchLevlUp: ({ display_name, level }) => `[ @${display_name} ${level} ]`,
   userDiscordLevlUp: ({ discord_id, display_name, level }) =>
-    `=[ ${(discord_id ? `<@${discord_id}> ` : '')}${display_name} ${level} ]=`,
+    `[ ${(discord_id ? `<@${discord_id}> ` : '')}${display_name} ${level} ]`,
   userDiscordPing: (user = {}) => user && user.discord_id ? ` <@${user.discord_id}> ` : '',
 
   xpBonusGroupDetails: ({ follower, discord, subscriber }) =>
