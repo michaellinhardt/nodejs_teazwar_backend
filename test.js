3c20e116a4756379e18d7a19c87b13cbae970b19
Michael Linhardt <linhardt.michael@gmail.com>
2021-10-25 10:38:57 +0700

    add test script to package.json

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
-const h = require('./helpers')
-const _ = require('lodash')
-const { render } = require('prettyjson')
+// const h = require('./helpers')
+// const _ = require('lodash')
+// const { render } = require('prettyjson')
+
+class testeuh {
+
+  onDelete () { console.debug('no on delete method') }
+
+}
+
+class testeuhh extends testeuh {
+  // onDelete () { console.debug('deleted!') }
+}
 
 const start = () => {
+  const test = new testeuhh()
+  test.onDelete()
   process.exit(0)
 }
 
