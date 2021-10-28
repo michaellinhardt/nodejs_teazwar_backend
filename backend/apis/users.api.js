e0bcc485fb6cc9fa25fc14169fe76c79dd086048
Michael Linhardt <linhardt.michael@gmail.com>
2021-10-29 05:40:57 +0700

    add getByUserId method

--- backend/apis/users.api.js
+++ backend/apis/users.api.js
@@ -4,8 +4,18 @@ import _ from 'lodash'
 const { twitch, apis: { endpoints } } = require('../../config')
 
 export default {
-  getByUsernames: async (chatterUsernames) => {
-    const usersUrl = `${endpoints.twitch.users}?login=${chatterUsernames.join('&login=')}`
+  getByUsernames: async (usernames) => {
+    const usersUrl = `${endpoints.twitch.users}?login=${usernames.join('&login=')}`
+    const response = await superagent
+      .get(usersUrl)
+      .set('Client-ID', twitch.clientId)
+      .set('Authorization', `Bearer ${twitch.oauth}`)
+      .set('Accept', 'application/json')
+      .catch((err) => console.error(err))
+    return _.get(response, 'body.data', [])
+  },
+  getByUserIds: async (userIds) => {
+    const usersUrl = `${endpoints.twitch.users}?id=${userIds.join('&id=')}`
     const response = await superagent
       .get(usersUrl)
       .set('Client-ID', twitch.clientId)
