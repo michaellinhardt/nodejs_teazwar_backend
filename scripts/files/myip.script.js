a542060ec4eef6ef38721b50cf521ded7af58a19
Michael Linhardt <linhardt.michael@gmail.com>
2021-10-29 12:53:37 +0700

    refacto script public ip

--- scripts/files/myip.script.js
+++ scripts/files/myip.script.js
@@ -2,9 +2,9 @@ const publicIp = require('public-ip')
 module.exports = async () => {
   await (async () => {
     const v6 = await publicIp.v6()
-    console.debug(v6)
+    console.info(v6)
     const v4 = await publicIp.v4()
-    console.debug(v4)
+    console.info(v4)
   })()
 }
 
